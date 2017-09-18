9be56a042cda60fad848843c3a3c8ee51af8d9d5
ed85 <ed85@users.noreply.github.com>
2017-09-19 01:08:30 +0200

    Add files via upload

---
+++ app/sprinkles/core/locale/valitron/ba.php
@@ -0,0 +1,33 @@
+<?php
+
+return array(
+    'required'      => "obavezno polje",
+    'equals'        => "mora biti identičan sa '%s'",
+    'different'     => "mora biti drugačiji od '%s'",
+    'accepted'      => "moraju biti prihvaćeni",
+    'numeric'       => "mora biti broj",
+    'integer'       => "mora biti cijeli broj",
+    'length'        => "kann nicht länger als %d sein",
+    'min'           => "mora biti minimalno%s",
+    'max'           => "ne smije biti više %s",
+    'in'            => "sadrži nevažeću vrijednost",
+    'notIn'         => "contains invalid value",
+    'ip'            => "nije važeća IP adresa",
+    'email'         => "nije važeća e-mail adresa",
+    'url'           => "nije URL",
+    'urlActive'     => "domena mora biti aktivna",
+    'alpha'         => "mora sadržavati samo slova a-z",
+    'alphaNum'      => "mora sadržavati samo slova a-z i/ili brojeve 0-9",
+    'slug'          => "mora sadržavati samo slova a-z, brojeve 0-9, '-' i '_'",
+    'regex'         => "sadrži pogrešne znakove",
+    'date'          => "nije ispravan datum",
+    'dateFormat'    => "mora biti datum sa formatom '%s'",
+    'dateBefore'    => "mora biti datum prije '%s'",
+    'dateAfter'     => "mora biti datum poslije '%s'",
+    'contains'      => "moraju sadržavati %s",
+    'boolean'       => "mora biti boolean",
+    'lengthBetween' => "mora biti između %d i %d znakova",
+    'creditCard'    => "kreditna kartica mora biti validna",
+    "lengthMin"     => "moraju sadržavati više od %d znakova",
+    "lengthMax"     => "mora sadržavati manje od %d znakova"
+);
